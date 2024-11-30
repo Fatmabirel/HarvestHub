7d410e43b67e4de95041ca5c9a131cf280065139
Fatma Birel <fatmabrl11@gmail.com>
2024-11-30 12:57:15 +0300

    Implement Generic Interface & Generic Repository

---
+++ DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,11 @@
+﻿namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDal<T> where T: class, new()
+    {
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        T GetById(int id);
+        List<T> GetListAll();
+    }
+}
---
+++ DataAccessLayer/Concrete/Repository/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repository
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class, new()
+    {
+        public void Add(T entity)
+        {
+            using var context = new AgricultureContext();
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            using var context = new AgricultureContext();
+            context.Remove(entity);
+            context.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            using var context = new AgricultureContext();
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll()
+        {
+            using var context = new AgricultureContext();
+            return context.Set<T>().ToList();
+        }
+
+        public void Update(T entity)
+        {
+            using var context = new AgricultureContext();
+            context.Update(entity);
+            context.SaveChanges();
+        }
+    }
+}
