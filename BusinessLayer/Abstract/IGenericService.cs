d26fbd8a655488a5438665e1880d5eb3d5b9da36
Fatma Birel <fatmabrl11@gmail.com>
2024-12-02 14:12:57 +0300

    Create Service & Manager on BusinessLayer

---
+++ BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,11 @@
+﻿namespace BusinessLayer.Abstract
+{
+    public interface IGenericService<T> where T : class, new()
+    {
+        void Add(T entity);
+        void Delete(T entity);
+        void Update(T entity);
+        T GetById(int id);
+        List<T> GetListAll();
+    }
+}
---
+++ BusinessLayer/Concrete/TeamManager.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace BusinessLayer.Concrete
+{
+    public class TeamManager : ITeamService
+    {
+        private readonly ITeamDal _teamDal;
+        public TeamManager(ITeamDal teamDal)
+        {
+            _teamDal = teamDal;
+        }
+
+        public void Add(Team entity)
+        {
+            _teamDal.Add(entity);
+        }
+
+        public void Delete(Team entity)
+        {
+            _teamDal.Delete(entity);
+        }
+
+        public Team GetById(int id)
+        {
+            return _teamDal.GetById(id);
+        }
+
+        public List<Team> GetListAll()
+        {
+            return _teamDal.GetListAll();
+        }
+
+        public void Update(Team entity)
+        {
+           _teamDal.Update(entity);
+        }
+    }
+}
