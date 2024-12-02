a74427094a4d9b6a139beacc4fdd4c5424d3bb32
Fatma Birel <fatmabrl11@gmail.com>
2024-12-02 18:36:44 +0300

    Implement Fluent Validation & Add Team Controller

---
+++ BusinessLayer/Validation Rules/TeamValidator.cs
@@ -0,0 +1,19 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+
+namespace BusinessLayer.Validation_Rules
+{
+    public class TeamValidator : AbstractValidator<Team>
+    {
+        public TeamValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("İsim alanı boş geçilemez!");
+            RuleFor(x => x.Name).MinimumLength(5).MaximumLength(30).WithMessage("İsim alanı 5 ile 30 karakter arasında olmalıdır!");
+
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Ünvan alanı boş geçilemez!");
+            RuleFor(x => x.Title).MinimumLength(3).MaximumLength(20).WithMessage("Ünvan alanı 3 ile 20 karakter arasında olmalıdır!");
+
+            RuleFor(x => x.ImageUrl).NotEmpty().WithMessage("Görsel alanı boş geçilemez!");
+        }
+    }
+}
---
+++ PresentationLayer/Controllers/TeamController.cs
@@ -0,0 +1,84 @@
+﻿using BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using EntityLayer.Concrete;
+using BusinessLayer.Validation_Rules;
+using FluentValidation.Results;
+
+namespace PresentationLayer.Controllers
+{
+    public class TeamController : Controller
+    {
+        private readonly ITeamService _teamService;
+        public TeamController(ITeamService teamService)
+        {
+            _teamService = teamService;
+        }
+
+        public IActionResult Index()
+        {
+            var values = _teamService.GetListAll();
+            return View(values);
+        }
+
+        [HttpGet] 
+        public IActionResult AddTeam()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddTeam(Team team)
+        {
+            ModelState.Clear();
+            TeamValidator validationRules = new TeamValidator();
+            ValidationResult result = validationRules.Validate(team);
+            if (result.IsValid)
+            {
+                _teamService.Add(team);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach(var item in result.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();
+            }            
+        }
+
+        public IActionResult DeleteTeam(int id)
+        {
+            var value = _teamService.GetById(id);
+            _teamService.Delete(value);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult EditTeam(int id)
+        {
+            var value = _teamService.GetById(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult EditTeam(Team team)
+        {
+            TeamValidator validationRules = new TeamValidator();
+            ValidationResult result = validationRules.Validate(team);
+            if (result.IsValid)
+            {
+                _teamService.Update(team);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var failure in result.Errors)
+                {
+                    ModelState.AddModelError(failure.PropertyName, failure.ErrorMessage);
+                }
+                return View(team);
+            }
+        }
+
+    }
+}
